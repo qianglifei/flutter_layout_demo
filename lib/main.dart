0ed6e1cbb6150232f6d5e6b9e2256c68cc4bcfda
qianglifei <qianglifei99>
2019-02-22 16:22:36 +0800

    Flutter 布局

--- lib/main.dart
+++ lib/main.dart
@@ -321,51 +321,157 @@ import 'package:flutter/material.dart';
 //    );
 //  }
 //}
+//
+////Dart 中可以使用类来抽象一个数据
+//class Product{
+//    //商品标题
+//    final String title;
+//    //商品描述
+//    final String description;
+//    Product(this.title,this.description);
+//}
+//
+//void main(){
+//    runApp(MaterialApp(
+//       title: '数据传递案例',
+//       home: ProductList(
+//          products:List.generate(
+//            20,
+//            (i) => Product('商品 $i','这是一个商品详情，编号为：$i'))
+//       ),
+//    ));
+//}
+//
+//class ProductList extends StatelessWidget{
+//  final List<Product> products;
+//  ProductList({Key key,@required this.products}):super(key:key);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//
+//    // TODO: implement build
+//    return Scaffold(
+//        appBar: new AppBar(
+//            title: Text('商品列表'),
+//        ),
+//        body: ListView.builder(
+//            itemCount: products.length,
+//            itemBuilder: (context,index){
+//                new Divider();
+//                return ListTile(
+//                  title: Text(products[index].title),
+//                  onTap: (){
+//                    Navigator.push(context, MaterialPageRoute(builder:
+//                        (context) => new ProductDetail(product:products[index])
+//                    ));
+//                  },
+//                );
+//            },
+//        ),
+//    );
+//  }
+//}
+//
+//class ProductDetail extends StatelessWidget{
+//  final Product product;
+//  ProductDetail({Key key,@required this.product}):super(key:key);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return Scaffold(
+//        appBar: new AppBar(
+//            title: Text('${product.title}'),
+//        ),
+//        body: Center(child: Text('${product.description}')),
+//    );
+//  }
+//}
 
-//Dart 中可以使用类来抽象一个数据
-class Product{
-    //商品标题
-    final String title;
-    //商品描述
-    final String description;
-    Product(this.title,this.description);
-}
+//void main(){
+//    runApp(MaterialApp(
+//        title: '页面跳转返回数据',
+//        home: FirstPage(),
+//    ));
+//}
+//
+//class FirstPage  extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return Scaffold(
+//        appBar: new AppBar(
+//            title: Text('找小姐姐要电话'),
+//        ),
+//        body: Center(
+//            child: RouteButton(),
+//        ),
+//    );
+//  }
+//}
+//
+//class RouteButton extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return RaisedButton(
+//          onPressed:() {
+//            _navigateToXiaoJieJie(context);
+//          },
+//          child: Text('去找小姐姐'),
+//      );
+//  }
+//
+//  _navigateToXiaoJieJie(BuildContext context) async{ // 是重启async
+//      final result = await Navigator.push(context, MaterialPageRoute(
+//          builder: (context) => XiaoJieJie()
+//      ));
+//
+//      Scaffold.of(context).showSnackBar(SnackBar(content: Text('$result')));
+//  }
+//}
+//
+//class XiaoJieJie extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return Scaffold(
+//        appBar: new AppBar(
+//            title: Text('我是小姐姐'),
+//        ),
+//        body: Center(
+//            child: Column(
+//                children: <Widget>[
+//                    RaisedButton(
+//                        child: Text('大长腿小姐姐'),
+//                        onPressed: (){
+//                            Navigator.pop(context,'大长腿，1882939238');
+//                        },
+//                    ),
+//                    RaisedButton(
+//                        child: Text('小蛮腰小姐姐'),
+//                        onPressed: (){
+//                            Navigator.pop(context,'小蛮腰，1512343455');
+//                        },
+//                    )
+//                ],
+//            ),
+//        ),
+//    );
+//  }
+//}
 
-void main(){
-    runApp(MaterialApp(
-       title: '数据传递案例',
-       home: ProductList(
-          products:List.generate(
-            20,
-            (i) => Product('商品 $i','这是一个商品详情，编号为：$i'))
-       ),
-    ));
-}
-
-class ProductList extends StatelessWidget{
-  final List<Product> products;
-  ProductList({Key key,@required this.products}):super(key:key);
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-        appBar: new AppBar(
-            title: Text('商品列表'),
-        ),
-        body: ListView.builder(
-            itemCount: products.length,
-            itemBuilder: (context,index){
-                new Divider();
-                return ListTile(
-                  title: Text(products[index].title),
-                  onTap: (){
-
-                  },
-
-                );
-            },
-        ),
-    );
-  }
-}
+//void main()=>runApp(MyApp());
+////**
+// * 静态资源和项目的图片处理
+// */
+//class MyApp extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return Container(
+//        child: Image.asset('images/ic_launcher.png'),
+//    );
+//  }
+//
+//}
